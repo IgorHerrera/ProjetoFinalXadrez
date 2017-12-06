fc275df56c793f49e7b05c53532e94a932d2b091
IgorHerrera <igorherrerasantos@gmail.com>
2017-12-06 11:12:56 -0200

    Class Posicao

--- ProjetoFinal/Program.cs
+++ ProjetoFinal/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
 namespace ProjetoFinal
 {
@@ -10,7 +7,15 @@ namespace ProjetoFinal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("AAAAAA");
+            Posicao P;
+
+            P = new Posicao(0,0);
+
+            Console.WriteLine("Posicao: " + P);
+
+            Console.ReadLine();
+
+
         }
     }
 }
