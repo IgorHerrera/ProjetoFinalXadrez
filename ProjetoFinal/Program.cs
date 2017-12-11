b2c124c8914ea1a7aa8b667d985287b29fae8e2e
Igor Herrera Santos <igorherrerasantos@gmail.com>
2017-12-11 17:13:54 -0200

    Excessao personalizada

--- ProjetoFinal/Program.cs
+++ ProjetoFinal/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace ProjetoFinal
 {
@@ -7,12 +8,22 @@ namespace ProjetoFinal
     {
         static void Main(string[] args)
         {
-            Posicao P;
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tela.imprimirTabuleiro(tab);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+ 
             Console.ReadLine();
 
         }
